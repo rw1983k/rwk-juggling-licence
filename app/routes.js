fba489d43eea78a7f752418640873eb57b4d08fd
rw1983k <rw1983k@gmail.com>
2019-10-11 15:00:37 +0100

    Adding routing

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,14 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.post('/juggling-balls-answer', function(req, res) {
+    var jugglingBalls = req.session.data['juggling-balls']
+
+    if(jugglingBalls == "5 or more") {
+        res.redirect('/juggling-tricks')
+    }
+    else {
+        res.redirect('/ineligible')
+    }
+})
